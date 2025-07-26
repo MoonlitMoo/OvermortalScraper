e0d8481c3f3f2f28e9c503555981b3edae9f220a
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-07-26 18:56:47 +1200

    fix: Update leaderboard to inject dependencies

--- leaderboard_scraper.py
+++ leaderboard_scraper.py
@@ -1,8 +1,13 @@
 from db.init import init_db
+from log import logger
 from scrapers.ranking_scraper import RankingScraper
+from scrapers.screenshot_processor import ScreenshotProcesser
+from screen import Screen
 
 session = init_db()
-scraper = RankingScraper(session)
+screen = Screen(logger)
+processer = ScreenshotProcesser()
+scraper = RankingScraper(screen, session, processer, logger)
 scraper.run()
 
 session.close()
