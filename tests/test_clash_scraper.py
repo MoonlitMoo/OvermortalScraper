336ee54933c0ccd1f9a52b26793ac2ee3f1c6317
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-08-17 18:28:24 +1200

    test: Tests for predictor + taoist index location

--- tests/test_clash_scraper.py
+++ tests/test_clash_scraper.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from core.log import logger
@@ -23,3 +24,17 @@ def test_get_opponent_br(scraper, caplog):
     brs = scraper.get_opponent_brs()
     assert scraper.own_br
     assert len(brs) == 5
+
+
+@save_log
+def test_basic_predictor(scraper, caplog):
+    enemy_brs = np.linspace(30e9, 60e9, 5).tolist()
+    scraper.own_br = 40e9
+    proba = scraper.basic_predict(enemy_brs)
+    assert all(proba)
+
+
+@save_log
+def test_get_taoist_location(scraper, caplog):
+    for i in range(5):
+        assert scraper.get_opponent_location(i)
